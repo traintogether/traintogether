9c996f5d6eb15f8389b2de34521ae0645e586b3c
Santosh Gogi <santoshag@gmail.com>
2016-09-06 12:39:43 -0700

    UI changes 1.0

--- TrainTogether/app/src/main/java/com/codepath/traintogether/adapters/EventsAdapter.java
+++ TrainTogether/app/src/main/java/com/codepath/traintogether/adapters/EventsAdapter.java
@@ -1,19 +1,5 @@
 package com.codepath.traintogether.adapters;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import com.codepath.traintogether.R;
-import com.codepath.traintogether.TrainTogetherApplication;
-import com.codepath.traintogether.models.Group;
-import com.codepath.traintogether.models.User;
-import com.codepath.traintogether.models.active.Place;
-import com.codepath.traintogether.models.active.Result;
-import com.codepath.traintogether.utils.Constants;
-import com.codepath.traintogether.utils.Utils;
-
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -22,6 +8,20 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.traintogether.R;
+import com.codepath.traintogether.TrainTogetherApplication;
+import com.codepath.traintogether.models.Group;
+import com.codepath.traintogether.models.User;
+import com.codepath.traintogether.models.active.Place;
+import com.codepath.traintogether.models.active.Result;
+import com.codepath.traintogether.utils.Constants;
+import com.codepath.traintogether.utils.Utils;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -33,7 +33,9 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView tvTitle;
         public TextView tvLocation;
+        public TextView tvCalendar;
         public TextView tvTime;
+
         public ImageView ivPoster;
         public ImageButton ibJoin;
 
@@ -47,6 +49,8 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
             tvTitle = (TextView) view.findViewById(R.id.tvTitle);
             tvLocation = (TextView) view.findViewById(R.id.tvLocation);
             tvTime = (TextView) view.findViewById(R.id.tvTime);
+            tvCalendar = (TextView) view.findViewById(R.id.tvCalendar);
+
             ivPoster = (ImageView) view.findViewById(R.id.ivPoster);
             ibJoin = (ImageButton) view.findViewById(R.id.ibJoin);
 
@@ -107,29 +111,33 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
         if (place != null) {
             holder.tvLocation.setText(
                     String.format(
-                            "%s, %s · %s",
+                            "%s, %s",
                             place.getCityName(),
-                            place.getCountryName(),
-                            Utils.getEventDate(event.getActivityStartDate())
+                            place.getCountryName()
                     )
             );
         } else {
             holder.tvLocation.setText("");
         }
+
+
+        holder.tvCalendar.setText(Utils.getEventDate(event.getActivityStartDate()));
         holder.tvTime.setText(Utils.getDaysToEvent(event.getActivityStartDate()));
 
         loadThumbnail(holder, place.getCityName());
     }
 
     private void loadThumbnail(ViewHolder holder, String cityName) {
+        int[] randBg = {R.drawable.mara1, R.drawable.mara2, R.drawable.mara3};
         if (cityName.equalsIgnoreCase("boston")) {
-            holder.ivPoster.setImageResource(R.drawable.boston);
+            Glide.with(getContext()).load(R.drawable.boston1).into(holder.ivPoster);
         } else if (cityName.equalsIgnoreCase("san francisco")) {
-            holder.ivPoster.setImageResource(R.drawable.san_francisco);
+            Glide.with(getContext()).load(R.drawable.san_francisco).into(holder.ivPoster);
         } else if (cityName.equalsIgnoreCase("san diego")) {
-            holder.ivPoster.setImageResource(R.drawable.san_diego);
+            Glide.with(getContext()).load(R.drawable.san_diego).into(holder.ivPoster);
         } else {
-            holder.ivPoster.setImageResource(R.drawable.default_marathon);
+
+            Glide.with(getContext()).load(randBg[Utils.randomWithRange(0,2)]).into(holder.ivPoster);
         }
     }
 
