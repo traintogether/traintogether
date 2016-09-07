6c0017b6d061ec91cba18dbb2b003fd793be3ecc
Santosh Gogi <santoshag@gmail.com>
2016-09-07 04:31:32 -0700

    splash screen added

--- TrainTogether/app/src/main/java/com/codepath/traintogether/activities/SplashActivity.java
+++ TrainTogether/app/src/main/java/com/codepath/traintogether/activities/SplashActivity.java
@@ -1,33 +1,54 @@
 package com.codepath.traintogether.activities;
 
-import com.codepath.traintogether.R;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.traintogether.R;
+import com.codepath.traintogether.utils.Utils;
 
 /**
  * Created by ameyapandilwar on 8/30/16 at 12:51 AM.
  */
-public class SplashActivity extends BaseActivity {
 
+public class SplashActivity extends AppCompatActivity {
+
+    /**
+     * Duration of wait
+     **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
 
-        Thread timer = new Thread() {
-            public void run() {
-                try {
-                    sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                }
-            }
-        };
-        timer.start();
-    }
+        TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
 
+        tvTitle.setText(Utils.getCustomTextForTitle(this, "Runathon", 200));
+
+        ImageView ivGraphic = (ImageView) findViewById(R.id.ivGraphic);
+        Glide.with(this).load(R.drawable.graphic).into(ivGraphic);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 }
